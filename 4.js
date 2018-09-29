7d69f16f67e1d1fd7b264e5bdb689ffe16b68e97
The5econd <00017917@uca.edu.sv>
2018-09-29 11:43:44 -0600

    ya funciona el ejercicio 4 para impares, TAREA COMPLETADA OFICIALMENTE

--- 4.js
+++ 4.js
@@ -1,11 +1,16 @@
 var array4 = [1,2,3,4,5,6]
 function fun4(array4){
-    for(let i = 0; i < array4.length; i++){
-        if ((array4.length) % 2 == 0 ){
-            console.log(array4.shift(i) + array4.pop(i));
+    for(let i = 0; i < array4.length + 1; i++){
+        if (array4.length % 2 != 0){
+            let h = ((array4.length/2)-0.5)
+            let j = array4.splice(h, 1);
+            console.log(array4.shift() + array4.pop());
+            
         }
-        else{
-            console.log(array4.shift(i) + array4.pop(i));
+
+        if ((array4.length) % 2 == 0 ){
+            console.log(array4.shift() + array4.pop());
         }
     }
+    console.log(j*2)
 }
